e37641e5502b19ab6ba00bcd4e99d354890f213f
nouman17610 <accanouman@gmail.com>
2023-08-01 07:31:17 -0700

    Enable Toggle Menu through Javascript

--- meetup.js
+++ meetup.js
@@ -1,10 +1,10 @@
-const hamIcon = document.querySelector('.menu-icon');
-const navItems = document.querySelector('.menu-list');
+const hamIcon = document.querySelector('.ham-icon');
+const navItems = document.querySelector('.nav-links');
 hamIcon.addEventListener('click', () => {
   hamIcon.classList.toggle('active');
   navItems.classList.toggle('active');
 });
-document.querySelectorAll('.menu-item a').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('.nav-item a').forEach((n) => n.addEventListener('click', () => {
   hamIcon.classList.remove('active');
   navItems.classList.remove('active');
   })
