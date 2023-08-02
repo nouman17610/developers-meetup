b5edf94777e1cfd1a950ee558247fbc47bdea947
nouman17610 <accanouman@gmail.com>
2023-08-02 12:38:00 -0700

    Correct CSS Linters

--- meetup.js
+++ meetup.js
@@ -16,57 +16,54 @@ closebutton.addEventListener("click", () => {
   navItems.classList.remove('active');
 });
 const featuredSpeakers = [
-    {
-      id: 1,
-      img: './images/speaker01.png',
-      name: 'Nouman Wali',
-      title: 'Fullstack Developer',
-      desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
-    },
-    {
-      id: 2,
-      img: './images/speaker02.png',
-      name: 'Nouman Wali',
-      title: 'Fullstack Developer',
-      desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
-    },
-    {
-      id: 3,
-      img: './images/speaker03.png',
-      name: 'Nouman Wali',
-      title: 'Fullstack Developer',
-      desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
-    },
-    {
-      id: 4,
-      img: './images/speaker04.png',
-      name: 'Nouman Wali',
-      title: 'Fullstack Developer',
-      desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
-    },
-    {
-      id: 5,
-      img: './images/speaker05.png',
-      name: 'Nouman Wali',
-      title: 'Fullstack Developer',
-      desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
-    },
-    {
-      id: 6,
-      img: './images/speaker06.png',
-      name: 'Nouman Wali',
-      title: 'Fullstack Developer',
-      desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
-    },
-  
-  ];
-  function addSpeakersToContainer(speakersData) {
-    const container = document.getElementById('speakers-container');
-  
-    speakersData.forEach((speaker) => {
-      const speakerCard = document.createElement('div');
-      speakerCard.classList.add('speaker-card');
-  
+  {
+    id: 1,
+    mg: './images/speaker01.png',
+    name: 'Nouman Wali',
+    title: 'Fullstack Developer',
+    desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
+  },
+  {
+    id: 2,
+    img: './images/speaker02.png',
+    name: 'Nouman Wali',
+    title: 'Fullstack Developer',
+    desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
+  },
+  {
+    id: 3,
+    img: './images/speaker03.png',
+    name: 'Nouman Wali',
+    title: 'Fullstack Developer',
+    desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
+  },
+  {
+    id: 4,
+    img: './images/speaker04.png',
+    name: 'Nouman Wali',
+    title: 'Fullstack Developer',
+    desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
+  },
+  {
+    id: 5,
+    img: './images/speaker05.png',
+    name: 'Nouman Wali',
+    title: 'Fullstack Developer',
+    desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
+  },
+  {
+    id: 6,
+    img: './images/speaker06.png',
+    name: 'Nouman Wali',
+    title: 'Fullstack Developer',
+    desc: 'Nouman wali handles a team of 50 developers and designers, he has an experience of around 10 years in programming.',
+  },
+];
+function addSpeakersToContainer(speakersData) {
+  const container = document.getElementById('speakers-container');
+  speakersData.forEach((speaker) => {
+    const speakerCard = document.createElement('div');
+    speakerCard.classList.add('speaker-card');
       // Separate div for the image
       const speakerImage = document.createElement('div');
       speakerImage.classList.add('speaker-image');
